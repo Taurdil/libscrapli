762a2936e2e03fc97ae708248b2110df08c4823f
Carl Montanari <carl.r.montanari@gmail.com>
2026-05-03 15:17:25 -0700

    fix: remove the deprecated trim processed for cli read any since this now is governed by get results options

--- src/cli.zig
+++ src/cli.zig
@@ -737,7 +737,6 @@ pub const Driver = struct {
         try res.record(
             .{
                 .rets = try self.session.readAny(allocator, options),
-                .trim_processed = false,
             },
         );
 
